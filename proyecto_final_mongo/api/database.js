90b26950bb2e39c84fb63a38b017a6ed1e18732a
Miguel Franco <francamente@hotmail.es>
2024-02-03 21:42:42 +0100

    añado variable de entorno

--- proyecto_final_mongo/api/database.js
+++ proyecto_final_mongo/api/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
+require("dotenv").config()
 
-mongoose.connect("mongodb+srv://maikelfranklin:MONGOmakelele-7@cluster0.vws5ljp.mongodb.net/proyecto_final_mongo")
-.then(db => {
-    console.log("Database connected on " + db.connection.host)
-}).catch(err => console.log(err))
+mongoose.connect(`mongodb+srv://maikelfranklin:${process.env.MONGO_PASSWORD}@cluster0.vws5ljp.mongodb.net/proyecto_final_mongo`)
+    .then(db => {
+        console.log("Database connected on " + db.connection.host)
+    }).catch(err => console.log(err))
 
